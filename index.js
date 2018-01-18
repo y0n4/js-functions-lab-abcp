1913de3ff7fc75a6b349ad490cac1f4ff8607f1a
yona l <04l.yona@gmail.com>
2018-01-18 05:54:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(holiday, name){
   return 'Happy ' + holiday + ', ' + name + '!';
 }
 
-function holidayCountDown(holiday, days){
+function holidayCountdown(holiday, days){
   return 'It\'s ' + days + ' days until ' + holiday + '!';
 }
